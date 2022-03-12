ec5f8842d3b4143705d6afb5181c7255b1a43c8b
hugosenari <hugosenari@gmail.com>
2022-03-12 00:11:05 -0300

    chore: re index nimplehttpserver

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''SimpleHTTPServer module based on net sockets'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimplehttpserver-master".url = "path:./master";
+  inputs."nimplehttpserver-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimplehttpserver-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''SimpleHTTPServer module based on net sockets'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-NimpleHTTPServer-master.flake = false;
+  inputs.src-NimpleHTTPServer-master.owner = "Hydra820";
+  inputs.src-NimpleHTTPServer-master.ref   = "refs/heads/master";
+  inputs.src-NimpleHTTPServer-master.repo  = "NimpleHTTPServer";
+  inputs.src-NimpleHTTPServer-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-NimpleHTTPServer-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-NimpleHTTPServer-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
